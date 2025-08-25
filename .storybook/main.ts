34427a9ea475d5f1c1f51447c8244d786f023566
AnaCarolinaMello <caldasdemelloanacarolina@gmail.com>
2025-08-25 17:56:30 -0300

    fix: minor changes

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -3,12 +3,7 @@ import { mergeConfig } from "vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
-  addons: [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions",
-    "@chromatic-com/storybook"
-  ],
+  addons: [],
   framework: {
     name: "@storybook/vue3-vite",
     options: {},
